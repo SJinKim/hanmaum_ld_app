3450fb4f9f3d83820c68ec0794525ffe32738d50
SeungJin <ksj_1207@hotmail.com>
2024-06-02 18:32:56 +0200

    Initial Commit

---
+++ HanmaumChurchApp/HanmaumChurchAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  HanmaumChurchAppApp.swift
+//  HanmaumChurchApp
+//
+//  Created by SeungJin Kim on 02.06.24.
+//
+
+import SwiftUI
+
+@main
+struct HanmaumChurchAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
